b88cd5a1dc0d4a64768e21825ec017549f692abd
10Xu <86581088+10XuQ@users.noreply.github.com>
2025-10-29 22:12:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from datetime import datetime, timezone
     author="Qwe(N)", # 插件作者
     desc="一个用于鉴定用户今日欧非指数和悲剧指数的插件，可直接通过关键词触发，限制每日每个群/私聊一次，并随机添加表情。", # 插件描述
     version="1.0.0", # 插件版本
-    repo="https://github.com/YourName/astrbot_plugin_oufei_jianding"
+    repo="https://github.com/10XuQ/astrbot_plugin_oufei_jianding"
 )
 class OuFeiJianDingPlugin(Star):
     """
